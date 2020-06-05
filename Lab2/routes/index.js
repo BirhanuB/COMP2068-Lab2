2690a012a384c9ab9285dfa4e2ca98c31cf598e2
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2020-06-04 20:36:35 -0400

    Added the calculations and error messages

--- Lab2/routes/index.js
+++ Lab2/routes/index.js
@@ -4,6 +4,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    console.log(req.query);
+    var n = Object.keys(req.query).length;
+    if (n === 3) {
+        var method = req.query.method;
+        var x = req.query.x;
+        var y = req.query.y;
+        if (method === "add") {
+            console.log(x + ' + ' + y + ' = ' + (Number(x) + Number(y)));
+        }
+        else if (method === "subtract") {
+            console.log(x + ' - ' + y + ' = ' + (x - y));
+        }
+        else if (method === "multiply") {
+            console.log(x + ' * ' + y + ' = ' + (x * y));
+        }
+        else if (method === "divide") {
+            console.log(x + ' / ' + y + ' = ' + (x / y));
+        }
+        else {
+            console.log("error: unrecognized method")
+        }
+    }
+    else if (n > 0 && n != 3) {
+        console.log('error: required parameter missing in the url');
+    }
     res.render('index', { title: 'Express' });
 });
 
